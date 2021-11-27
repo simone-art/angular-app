8606488e0148642b938fe739109d5d743e6b03ea
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-11-27 12:40:03 -0300

    Criada interface ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { PostViewComponent } from './post-view/post-view.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PostListComponent } from './post-list/post-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'not-found', component: NotFoundComponent},
+  { path: 'posts', component: PostListComponent},
+  { path: 'post/:id', component: PostViewComponent},
+  { path: 'sobre-mim', component: AboutComponent},
+  { path: 'contato', component: ContactComponent},
+  { path: 'portifolio', component: PortifolioComponent},
+  {path: '', redirectTo: '/posts', pathMatch: 'full'},
+  {path: '**', redirectTo: '/not-found', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/post.interface.ts
@@ -0,0 +1,10 @@
+export interface Post{
+  id: string;
+  title: string;
+  category: string;
+  likes: number;
+  autor?: string;
+  tags?: string[];
+  created: string;
+}
+
